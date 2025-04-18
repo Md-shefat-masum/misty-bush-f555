47fe8e7d915112f2f94428d3a5e780f9721efd2b
Md. Shefat <mshefat924@gmail.com>
2025-04-19 01:23:50 +0600

    Create About.tsx

---
+++ app/routes/About.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <div>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a>
+      </div>
+       About me
+    </div>
+  );
+}
